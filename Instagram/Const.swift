4d2fe7323af44ccbb43b6b8f6bdafc791fb7c760
umeriko <rikakoster2@gmail.com>
2018-02-03 20:04:08 +0900

    最初のコミット

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 梅下理香子 on 2018/02/03.
+//  Copyright © 2018年 rikako.umeshita. All rights reserved.
+//
+
+import Foundation
+
+
+struct Const {
+    static let PostPath = "Posts"
+}
